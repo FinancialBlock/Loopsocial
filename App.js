9b85feb770835f089452fae9401e9ee9b57dc007
App Universal <app.universal.company@gmail.com>
2021-06-25 00:11:29 -0500

    Add user database

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
-import { withAuthenticator } from 'aws-amplify-react-native'
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {Node} from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {
@@ -24,7 +23,58 @@ import {
 import Home from './src/screens/Home';
 import Navigation from './src/navigation';
 
-const App: () => Node = () => {
+import {Auth, API, graphqlOperation} from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native';
+
+import {createUser} from './src/graphql/mutations';
+import {getUser} from './src/graphql/queries';
+
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)];
+};
+
+const App: () => React$Node = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get currently authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      if (!userInfo) {
+        return;
+      }
+
+      // check if THE user exist in database
+      const getUserResponse = await API.graphql(
+        graphqlOperation(getUser, {id: userInfo.attributes.sub}),
+      );
+
+      if (getUserResponse.data.getUser) {
+        console.log('User already exists in database');
+        return;
+      }
+
+      // if it doesn't (it's newly registered user)
+      // then, create a new user in database
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage(),
+      };
+
+      await API.graphql(graphqlOperation(createUser, {input: newUser}));
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
       <Navigation />
